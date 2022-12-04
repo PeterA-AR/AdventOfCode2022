f966be88f0ce1c81df8a38995efbcacaf9dc3963
Peter Armstrong <peter.armstrong@autorek.com>
2022-12-04 11:49:51 +0000

    Day 4 Part 2

--- Day4/Day4.cs
+++ Day4/Day4.cs
@@ -13,15 +13,46 @@ namespace AdventOfCode
             return (int.Parse(pair.Split('-')[0]), int.Parse(pair.Split('-')[1]));
         }
 
-        private static bool isPairOverlap(string pair1, string pair2)
+        private static bool isPairOverlapEntirely(string pair1, string pair2)
         {
-            var pair1Parts = pairParts(pair1);
-            var pair2Parts = pairParts(pair2);
+            var p1 = pairParts(pair1);
+            var p2 = pairParts(pair2);
 
-            return (pair1Parts.Item1 >= pair2Parts.Item1 && pair1Parts.Item2 <= pair2Parts.Item2) || (pair2Parts.Item1 >= pair1Parts.Item1 && pair2Parts.Item2 <= pair1Parts.Item2);
+            return (p1.Item1 >= p2.Item1 && p1.Item2 <= p2.Item2) || (p2.Item1 >= p1.Item1 && p2.Item2 <= p1.Item2);
         }
 
         public static void PartOne()
+        {
+            List<string> lines = File.ReadAllLines("input.txt").ToList();
+            int count = 0;
+            foreach (string line in lines)
+            {
+                Console.Write(line);
+                string[] split = line.Split(',');
+                string pair1 = split[0];
+                string pair2 = split[1];
+                if (isPairOverlapEntirely(pair1, pair2))
+                {
+                    count++;
+                    Console.Write(" - overlaps");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Number of Entirely Overlapping Pairs: " + count);
+        }
+
+        private static bool isPairOverlap(string pair1, string pair2)
+        {
+            var p1 = pairParts(pair1);
+            var p2 = pairParts(pair2);
+
+            bool overlapsEntirely = isPairOverlapEntirely(pair1, pair2);
+
+            return overlapsEntirely || (p1.Item2 <= p2.Item1 || p1.Item1 <= p2.Item2) && (p2.Item2 <= p1.Item1 || p2.Item1 <= p1.Item2);
+        }
+
+        private static void PartTwo()
         {
             List<string> lines = File.ReadAllLines("input.txt").ToList();
             int count = 0;
@@ -44,7 +75,8 @@ namespace AdventOfCode
 
         public static void main()
         {
-            PartOne();
+            //PartOne();
+            PartTwo();
         }
     }
 }
